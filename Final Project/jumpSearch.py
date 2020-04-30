a82ba66a831da4fe6d0b5dc84633c14efbbd00ee
Andy <iliescuandrew@gmail.com>
2020-04-30 14:41:32 -0500

    Updated the Jump search algorithm and added a test to verify the run time based on the list size

---
+++ Final Project/jumpSearch.py
@@ -0,0 +1,28 @@
+import math
+
+def jump(lst, val, size):
+    #Determines the step size for how many elements to jump over 
+    step = math.sqrt(size)
+    #Keeps track of the block we are on
+    track = 0
+
+    #Searching to see which block of size sqrt(size) would contain the value
+    while lst[int(step)] < val:
+        track = int(step)
+        step += step
+        #Reaching the end of list without finding the value
+        if track >= size:
+            return -1
+
+    #If step is > size we are checking the previous blocl for the value
+    while lst[int(track)] < val:
+        track+=1
+        #If we checked whole block unsuccessfully
+        if track > val:
+            return -1
+    
+    #If value is found in the list return it
+    if lst[int(track)] == val:
+        return track
+    
+    return -1
